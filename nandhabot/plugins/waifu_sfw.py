c985f9958eb91c3b8b1d3ce3efd623e470dbe1b0
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-01 19:54:20 +0530

    Update waifu_sfw.py

--- nandhabot/plugins/waifu_sfw.py
+++ nandhabot/plugins/waifu_sfw.py
@@ -3,8 +3,8 @@ from pyrogram import filters
 from pyrogram.types import Message 
 import requests 
 
-@bot.on_message(filters.command("angry"))
+@bot.on_message(filters.command("kill"))
 def waifu(_, m: Message):
-    api = request.get("https://api.waifu.pics/sfw/angry").json()
+    api = request.get("https://api.waifu.pics/sfw/kill").json()
     url = api["url"]
-    m.reply_animation(url)
+    m.reply_photo(url)
