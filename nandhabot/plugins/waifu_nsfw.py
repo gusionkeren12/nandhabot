1efc1edb2b61477e4d09a7b24b961ca3b8a6fc0e
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-01 19:15:54 +0530

    Update waifu_nsfw.py

--- nandhabot/plugins/waifu_nsfw.py
+++ nandhabot/plugins/waifu_nsfw.py
@@ -1,3 +1,11 @@
 from nandhabot import bot
 from pyrogram import filters
 from pyrogram.types import Message
+import requests 
+
+@bot.on_message(filtees.command("hneko"))
+def hneko(_, m: Message):
+    api = requests.get("https://api.waifu.pics/nsfw/neko").json()
+    url = api["url"]
+    m.reply_photo(photo=url)
+    
