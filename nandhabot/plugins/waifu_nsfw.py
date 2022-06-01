63f27391897cf582acf3a396a34e914b29ebacff
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-01 19:26:06 +0530

    Update waifu_nsfw.py

--- nandhabot/plugins/waifu_nsfw.py
+++ nandhabot/plugins/waifu_nsfw.py
@@ -3,27 +3,27 @@ from pyrogram import filters
 from pyrogram.types import Message
 import requests 
 
-@bot.on_message(filtees.command("hneko"))
+@bot.on_message(filters.command("hneko"))
 def hneko(_, m: Message):
     api = requests.get("https://api.waifu.pics/nsfw/neko").json()
     url = api["url"]
     m.reply_animation(url)
     
-@bot.on_message(filtees.command("hwaifu"))
+@bot.on_message(filters.command("hwaifu"))
 def hwaifu(_, m: Message):
     api = requests.get("https://api.waifu.pics/nsfw/waifu").json()
     url = api["url"]
     m.reply_animation(animation=url)
    
 
-@bot.on_message(filtees.command("trap"))
+@bot.on_message(filters.command("trap"))
 def trap(_, m: Message):
     api = requests.get("https://api.waifu.pics/nsfw/trap").json()
     url = api["url"]
     m.reply_animation(url)
     
 
-@bot.on_message(filtees.command("blowjob"))
+@bot.on_message(filters.command("blowjob"))
 def blowjob(_, m: Message):
     api = requests.get("https://api.waifu.pics/nsfw/blowjob").json()
     url = api["url"]
