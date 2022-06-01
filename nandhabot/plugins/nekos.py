7a3ee5d78994960416275a4f376ac334813889b4
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-01 18:18:03 +0530

    Update nekos.py

--- nandhabot/plugins/nekos.py
+++ nandhabot/plugins/nekos.py
@@ -7,20 +7,9 @@ from nandhabot import bot
 from pyrogram import filters
 from pyrogram.types import Message
 
-"""
-Working cmds:
-/neko
-/wallpaper
-/waifu
-/smug
-/kiss
-/foxgirl
-/gasm
-/tickle
-/ngif
-/feed
-/cuddle
-"""
+
+
+
 
 @bot.on_message(filters.command("feed"))
 def feed(_,  m: Message):
@@ -107,47 +96,3 @@ def baka(_,  m: Message):
     m.reply_photo(nekos.img(target))
 
     
-@bot.on_message(filters.command('wink'))
-def wink(_,message):
-    reply = message.reply_to_message
-    if reply:
-        res = requests.get('https://some-random-api.ml/animu/wink').json()
-        url = res['link']
-        reply.reply_animation(url)
-        
-    else:
-        message.reply_animation(url)
-        
-        
-@bot.on_message(filters.command('hug'))
-def hug(_,message):
-    reply = message.reply_to_message
-    if reply:
-        res = requests.get('https://some-random-api.ml/animu/hug').json()
-        url = res['link']
-        reply.reply_animation(url)
-        
-    else:
-        message.reply_animation(url)
-
-@bot.on_message(filters.command('pat'))
-def pat(_,message):
-    reply = message.reply_to_message
-    if reply:
-        res = requests.get('https://some-random-api.ml/animu/pat').json()
-        url = res['link']
-        reply.reply_animation(url)
-        
-    else:
-        message.reply_animation(url)
-
-@bot.on_message(filters.command("slap"))
-def slap(_, message: Message):
-    reply = message.reply_to_message
-    if reply:
-        res = requests.get("https://api.rei.my.id/v3/slap").json()
-        url = res["url"]
-        reply.reply_animation(url)
-        
-    else:
-          message.reply_animation(url)
