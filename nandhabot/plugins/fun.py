d565e7be3c7428deefad0b78ddbd848498a55cec
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-01 18:14:50 +0530

    Update fun.py

--- nandhabot/plugins/fun.py
+++ nandhabot/plugins/fun.py
@@ -1,6 +1,6 @@
 from pyrogram.types import Message
 from pyrogram import filters
-from nksama import bot, BOT_ID
+from nandhabot import bot, BOT_ID
 import random
 import requests 
         
@@ -40,7 +40,7 @@ async def gbams(_, m: Message):
       if not reply:
        return await m.reply("reply someone:\n/gban or /gbam")
       user1 = m.from_user
-      reason = m.text.split(None, 1)[1] or "None"
+      reason = m.text.split(None, 1)[1]
       count = random.randint(10,30)
       user2 = reply.from_user
       chat = m.chat
