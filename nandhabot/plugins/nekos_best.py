a08b079472df64a97bfc8827c9d7135ba801e18f
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-02 16:56:14 +0530

    Update nekos_best.py

--- nandhabot/plugins/nekos_best.py
+++ nandhabot/plugins/nekos_best.py
@@ -2,6 +2,167 @@ from nandhabot import bot #nekos.best
 from pyrogram import filters 
 from pyrogram.types import Message 
 import requests 
+"""
+Images (.png)
+kitsune, neko, waifu
+
+GIFs (.gif)
+baka, bite, 
+blush, bored,
+cry, cuddle, dance, 
+facepalm, feed, 
+handhold, happy, 
+highfive, hug, kick,kiss,
+laugh, pat, poke, pout, 
+punch, shoot, shrug, slap, 
+sleep, smile, smug, stare, 
+think, thumbsup, tickle,
+wave, wink
+"""
+@bot.on_message(filters.command("dance"))
+def dance(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/dance").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/dance").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+@bot.on_message(filters.command("baka"))
+def baka(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/baka").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/baka").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+@bot.on_message(filters.command("bore"))
+def bore(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/bored").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/bored").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+
+@bot.on_message(filters.command("laugh"))
+def laugh(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/laugh").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/laugh").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+@bot.on_message(filters.command("smug"))
+def smug(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/smug").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/smug").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+@bot.on_message(filters.command("thumbsup"))
+def thumbsup(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/thumbsup").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/thumbsup").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+@bot.on_message(filters.command("shoot"))
+def shoot(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/shoot").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/shoot").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+@bot.on_message(filters.command("tickle"))
+def tickle(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/tickle").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/tickle").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+@bot.on_message(filters.command("feed"))
+def feed(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/feed").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/feed").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+@bot.on_message(filters.command("think"))
+def think(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/think").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/think").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+@bot.on_message(filters.command("wink"))
+def wink(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/wink").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/wink").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
+
+@bot.on_message(filters.command("sleep"))
+def sleep(_, m: Message):
+      reply = m.reply_to_message
+      if reply:
+          api = requests.get("https://nekos.best/api/v2/sleep").json()
+          url = api["url"]
+          reply.reply_animation(url)
+      else:
+          api = requests.get("https://nekos.best/api/v2/sleep").json()
+          url = api["url"]
+          m.reply_animation(animation=url)
 
 @bot.on_message(filters.command("punch"))
 def punch(_, m: Message):
