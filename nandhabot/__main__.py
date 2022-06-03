3771d2ac190c0bbf4f15637e43f3e73cc9c929de
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-03 17:32:33 +0530

    Update __main__.py

--- nandhabot/__main__.py
+++ nandhabot/__main__.py
@@ -26,7 +26,7 @@ PYRO - {}
 """
 
 @bot.on_message(filters.user(dev_user) & filters.command("alive"))
-def alive(_, m: Message):
+def async alive(_, m: Message):
     await msg = m.reply_text("Initialising")
     await msg.edit("Initialising ✪●●●●●")
     time.sleep(1)
