5446010bfc9bb82b342cc1d44264cbdf3d594f4f
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-03 07:45:56 +0530

    Update dev_user.py

--- nandhabot/plugins/dev_user.py
+++ nandhabot/plugins/dev_user.py
@@ -43,7 +43,7 @@ def get_readable_time(seconds: int) -> str:
 @bot.on_message(filters.command("pin")) 
 def pin(_, m):
   message_id = m.reply_to_message.id
-  admin = bot.get_chat_members(m.chat.id, 
+  admin = bot.get_chat_member(m.chat.id, 
                                m.from_user.id,  
                                filter='administrators')
   if m.from_user.id in admin:
