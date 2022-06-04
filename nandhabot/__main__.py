fac154d212fd51b2279b8ac37c0e3ee5e4712845
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-04 20:17:14 +0530

    Update __main__.py
    h

--- nandhabot/__main__.py
+++ nandhabot/__main__.py
@@ -1,10 +1,7 @@
 from nandhabot import bot
-from pyrogram import filters, __version__ as pyro
-from pyrogram.types import Message 
 import logging
 import nandhabot.plugins
-from nandhabot.config import SUPPORT_CHAT, BOT_USERNAME
-from nandhabot import dev_user
+from nandhabot.config import SUPPORT_CHAT
 
 logging.basicConfig(
     filename="logs.txt",
@@ -19,26 +16,3 @@ if __name__ == "__main__":
     with bot:
         bot.send_message(f"@{SUPPORT_CHAT}" , "Hello there I'm Now online")
         
-alive = """
-BOT - {}
-DEV - @NandhaxD
-PYRO - {}
-"""
-
-@bot.on_message(filters.command("alive"))
-async def alive(_, m: Message):
-    msg = await m.reply_text("Initialising")
-    await msg.edit("Initialising ✪●●●●●")
-    time.sleep(1)
-    await msg.edit("Initialising ✪✪●●●●")
-    time.sleep(1)
-    await msg.edit("Initialising ✪✪✪●●●")
-    time.sleep(1)
-    await msg.edit("Initialising ✪✪✪✪●●")
-    time.sleep(1)
-    await msg.edit("Initialising ✪✪✪✪✪●")
-    time.sleep(1)
-    await msg.edit("Initialising ✪✪✪✪✪✪")
-    time.sleep(1)
-    await msg.edit("✪︎Connection Successful✪")
-    await msg.edit(alive.format(BOT_USERNAME, pyro))
