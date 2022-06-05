fc6ddd5a28fc9a6506703d530b313079c7fea672
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-05 23:02:55 +0530

    Update memeapis.py

--- nandhabot/plugins/memeapis.py
+++ nandhabot/plugins/memeapis.py
@@ -42,3 +42,42 @@ async def ameme(_, query: CallbackQuery):
             ],
         ),
     )
+                
+@bot.on_message(filters.command("meme"))
+async def meme(_, m):
+     res = requests.get("https://meme-api.herokuapp.com/gimme/memes").json()
+     url = res['url']
+     text = res['title']
+     link = res['postLink']
+     await m.reply_photo(url,caption=f"[{text}]({link})",reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Change 🔂",
+                        callback_data="meme",
+                    ),
+                ],
+            ],
+        ),
+    )
+        
+@bot.on_callback_query(filters.regex("meme"))
+async def ameme(_, query: CallbackQuery):
+                   query = query.message
+                   await query.delete()
+                   res = requests.get("https://meme-api.herokuapp.com/gimme/memes").json()
+                   url = res['url']
+                   text = res['title']
+                   link = res['postLink']
+                   await query.reply_photo(url,caption=f"[{text}]({link})",reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Change 🔂",
+                        callback_data="meme",
+                    ),
+                ],
+            ],
+        ),
+    )
+
