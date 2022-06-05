806226c500353c369d5632e537f614ead6f3cf6a
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-05 11:45:53 +0530

    Update __init__.py

--- nandhabot/__init__.py
+++ nandhabot/__init__.py
@@ -26,7 +26,7 @@ API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
 
-bot = Client("nandhabot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
+bot = Client("nandhabot", api_id=API_ID, api_hash=API_HASH)
 
 print("Starting bot client")
 bot.start()
