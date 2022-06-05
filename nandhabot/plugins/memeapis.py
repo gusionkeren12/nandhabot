f5568d116d4b80ab34b61633f898cd45e3401265
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-05 22:54:21 +0530

    Create memeapis.py

---
+++ nandhabot/plugins/memeapis.py
@@ -0,0 +1,44 @@
+from pyrogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import filters
+from nandhabot import bot
+import random
+import requests 
+        
+        
+@bot.on_message(filters.command(["ameme","animememe"]))
+async def animememe(_, m):
+     res = requests.get("https://meme-api.herokuapp.com/gimme/animememes").json()
+     url = res['url']
+     text = res['title']
+     link = res['postLink']
+     await m.reply_photo(url,caption=f"[{text}]({link})",reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Change 🔂",
+                        callback_data="ameme",
+                    ),
+                ],
+            ],
+        ),
+    )
+        
+@bot.on_callback_query(filters.regex("ameme"))
+async def ameme(_, query: CallbackQuery):
+                   query = query.message
+                   await query.delete()
+                   res = requests.get("https://meme-api.herokuapp.com/gimme/animememes").json()
+                   url = res['url']
+                   text = res['title']
+                   link = res['postLink']
+                   await query.reply_photo(url,caption=f"[{text}]({link})",reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Change 🔂",
+                        callback_data="ameme",
+                    ),
+                ],
+            ],
+        ),
+    )
