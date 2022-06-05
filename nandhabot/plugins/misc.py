b4f18c754356afec76a09d5c87f4a91216f8f93a
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-05 06:39:57 +0530

    Xd

--- nandhabot/plugins/misc.py
+++ nandhabot/plugins/misc.py
@@ -21,11 +21,11 @@ async def reddit(_, m: Message):
            await m.reply(" /reddit {query}")
            query = m.text.split(None, 1)[1]
            if query:
-           x = await arq.reddit(query)
-           y = x.result
-           msg = await m.reply("searching now")
-           await msg.delete()
-           await m.reply_photo(
+                 x = await arq.reddit(query)
+                 y = x.result
+                msg = await m.reply("searching now")
+                await msg.delete()
+                await m.reply_photo(
                             y.url,  caption=f"[{y.title}]({y.postLink})")
 
 
