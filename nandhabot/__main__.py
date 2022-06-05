e9844cdbde778ebab2f90b2fd387cf82da87e5c0
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-05 11:53:02 +0530

    Update __main__.py

--- nandhabot/__main__.py
+++ nandhabot/__main__.py
@@ -11,6 +11,37 @@ logging.basicConfig(
     datefmt="%H:%M:%S",
 )
 
+HELPABLE = {}
+
+
+async def start_bot():
+    global HELPABLE
+
+    for module in ALL_MODULES:
+        imported_module = importlib.import_module("nandhabot.plugins." + module)
+        if (
+                hasattr(imported_module, "__MODULE__")
+                and imported_module.__MODULE__
+        ):
+            imported_module.__MODULE__ = imported_module.__MODULE__
+            if (
+                    hasattr(imported_module, "__HELP__")
+                    and imported_module.__HELP__
+            ):
+                HELPABLE[
+                    imported_module.__MODULE__.replace(" ", "_").lower()
+                ] = imported_module
+    bot_modules = ""
+    j = 1
+    for i in ALL_MODULES:
+        if j == 4:
+            bot_modules += "|{:<15}|\n".format(i)
+            j = 0
+        else:
+            bot_modules += "|{:<15}".format(i)
+        j += 1
+
+
 if __name__ == "__main__":
     bot.run()
     with bot:
