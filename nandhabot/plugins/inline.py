7ac57eedd1cef8a156d7957a00091ffccf86f4fc
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-05 22:53:06 +0530

    Update inline.py

--- nandhabot/plugins/inline.py
+++ nandhabot/plugins/inline.py
@@ -21,7 +21,7 @@ Powerd by @PegaBots
 @bot.on_inline_query()
 async def inline_query_handler(client, query):
     string = query.query.lower()
-    if string == "":
+    if text.split()[0] == "alive":
         await client.answer_inline_query(
             query.id,
             results=[
