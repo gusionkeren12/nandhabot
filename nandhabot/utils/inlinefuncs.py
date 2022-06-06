ea91b76b09079a7a774486b6cc41aa4e1ff59e2a
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-06 06:33:36 +0530

    Create inlinefuncs.py

---
+++ nandhabot/utils/inlinefuncs.py
@@ -0,0 +1,53 @@
+
+
+
+keywords_list = [
+    "image",
+    "wall",
+    "tmdb",
+    "lyrics",
+    "exec",
+    "speedtest",
+    "search",
+    "ping",
+    "tr",
+    "ud",
+    "yt",
+    "info",
+    "google",
+    "torrent",
+    "wiki",
+    "music",
+    "ytmusic",
+]
+
+
+async def inline_help_func(__HELP__):
+    buttons = InlineKeyboard(row_width=4)
+    buttons.add(
+        *[
+            (InlineKeyboardButton(text=i, switch_inline_query_current_chat=i))
+            for i in keywords_list
+        ]
+    )
+    answerss = [
+        InlineQueryResultArticle(
+            title="Inline Commands",
+            description="Help Related To Inline Usage.",
+            input_message_content=InputTextMessageContent(
+                "Click A Button To Get Started."
+            ),
+            thumb_url="https://hamker.me/cy00x5x.png",
+            reply_markup=buttons,
+        ),
+        InlineQueryResultArticle(
+            title="Github Repo",
+            description="Get Github Respository Of Bot.",
+            input_message_content=InputTextMessageContent(
+                "https://github.com/thehamkercat/WilliamButcherBot"
+            ),
+            thumb_url="https://hamker.me/gjc9fo3.png",
+        ),
+    ]
+    answerss = await alive_function(answerss)
+    return answerss
