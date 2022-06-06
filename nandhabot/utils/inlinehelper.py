c5dbb8d456c79ba678718d4e74416c644d8658b7
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-06 06:13:49 +0530

    Create inlinehelper.py

---
+++ nandhabot/utils/inlinehelper.py
@@ -0,0 +1,21 @@
+async def wall_func(answers, text):
+    results = await arq.wall(text)
+    if not results.ok:
+        answers.append(
+            InlineQueryResultArticle(
+                title="Error",
+                description=results.result,
+                input_message_content=InputTextMessageContent(results.result),
+            )
+        )
+        return answers
+    results = results.result[0:48]
+    for i in results:
+        answers.append(
+            InlineQueryResultPhoto(
+                photo_url=i.url_image,
+                thumb_url=i.url_thumb,
+                caption=f"[Source]({i.url_image})",
+            )
+        )
+    return answers
