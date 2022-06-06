9e95c61a7f8a28664d66409f6979b8d188bc7eed
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-06 06:40:56 +0530

    Update inlinebot.py

--- nandhabot/plugins/inlinebot.py
+++ nandhabot/plugins/inlinebot.py
@@ -1,1 +1,19 @@
+import traceback
 
+from nandhabot import bot as app
+from nandhabot.utils.inlinefuncs import *
+
+__MODULE__ = "Inline"
+__HELP__ = """See inline for help related to inline"""
+
+
+@app.on_inline_query()
+async def inline_query_handler(client, query):
+    try:
+        text = query.query.strip().lower()
+        answers = []
+        if text.strip() == "":
+            answerss = await inline_help_func(__HELP__)
+            await client.answer_inline_query(
+                query.id, results=answerss, cache_time=10
+            )
