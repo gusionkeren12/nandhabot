c6d709b7fa318a3ebb81870ffca92e38dbe69103
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-11 09:44:46 +0530

    Update admin.py

--- nandhabot/plugins/admin.py
+++ nandhabot/plugins/admin.py
@@ -1,1 +1,20 @@
+from pyrogram import filters
+from nandhabot import bot
 
+
+async def is_admins(chat_id: int):
+    return [
+        member.user.id
+        async for member in app.iter_chat_members(
+            chat_id, filter="administrators"
+        )
+    ]
+
+@bot.on_message(filters.command("ban"))
+async def ban(_, m):
+     reply = m.reply_to_message
+     if not is_admin:
+          await m.reply("your not admin!")
+     if m.from_user.id in is_admin:
+          await bot.ban_chat_member(m.chat.id, reply.from_user.id)
+          await m.reply_text(f"bammed! {reply.from_user.id}")
