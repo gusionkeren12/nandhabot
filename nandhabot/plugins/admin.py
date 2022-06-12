fa250c7413849a4b0d1ad6e4eca6f5cc98f4fecd
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-12 06:58:00 +0530

    Update admin.py

--- nandhabot/plugins/admin.py
+++ nandhabot/plugins/admin.py
@@ -13,8 +13,14 @@ async def is_admins(chat_id: int):
 @bot.on_message(filters.command("ban"))
 async def ban(_, m):
      reply = m.reply_to_message
-     if not is_admins:
-          await m.reply("your not admin!")
-     if m.from_user.id in is_admins:
-          await bot.ban_chat_member(m.chat.id, reply.from_user.id)
-          await m.reply_text(f"bammed! {reply.from_user.id}")
+     user = m.from_user.id
+     chat = m.chat
+     if user not in (
+            await is_admins(chat_id)
+        ):
+            return await message.reply_text(
+                "You are not admin"
+            )
+     if user in await is_admins(chat_id)):
+         await bot.ban_chat_member(m.chat.id, reply.from_user.id)
+         await m.reply_text(f"bammed! {reply.from_user.id}")
