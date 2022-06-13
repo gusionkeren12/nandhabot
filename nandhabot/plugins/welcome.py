ce2365d2f1cb9c66019dab241d7af3cbe14053b3
Aasf-Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2022-06-13 13:11:07 +0530

    Update welcome.py

--- nandhabot/plugins/welcome.py
+++ nandhabot/plugins/welcome.py
@@ -10,14 +10,14 @@ WEL_GIF = "https://telegra.ph/file/b616d3a126715d9b1aa46.mp4"
 async def welcome(_, m: Message):
     await m.reply_animation(
         WEL_GIF,
-        caption="hello! dear {}\nWelcome **{}**!".format(
+        caption="**Hello! Dear **{}\n**Welcome To****{}**!".format(
             m.from_user.mention, m.chat.title
-        ),
+        )
     )
 
 
 @bot.on_message(filters.left_chat_member)
 async def member_has_left(_, m: Message):
-    await m.reply(
-        "oof! dear {}\nlefted **{}**".format(m.from_user.mention, m.chat.title)
+    await m.reply_text(
+        "`Oof! Dear` {}\n`Lefted On` **{}**".format(m.from_user.mention, m.chat.title)
     )
