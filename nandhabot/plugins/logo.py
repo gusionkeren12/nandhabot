185294997f8bdb5c0256a48dd292a7d1f988712e
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-14 00:25:18 +0530

    Create logo.py

---
+++ nandhabot/plugins/logo.py
@@ -0,0 +1,10 @@
+from nandhbot import bot
+from pyrogram import filters
+import requests 
+
+
+@bot.on_message(filters.command("logo"))
+async def logo(_, m):
+             text = message.text.split(None, 1)[1]
+             logo = get((f"https://single-developers.up.railway.app/logo?name={text}").replace(' ','%20')).history[1].url
+             await m.reply_photo(logo,caption="Made by @VegetaRobot")
