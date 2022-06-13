c2439a01b0b7724d6a735b7e03debe78f5070a8d
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-13 09:13:35 +0530

    Update countryinfo.py

--- nandhabot/plugins/countryinfo.py
+++ nandhabot/plugins/countryinfo.py
@@ -7,17 +7,89 @@ from pyrogram import filters
 
 @bot.on_message(filters.command(["ci", "countryinfo"]))
 async def countryinfo(_, m):
-                  search = m.text.split(None, 1)[1]
-                  country = CountryInfo(search)
-                  a = country.info()
-                  name = a.get("name")
-                  capital = a.get("capital")
-                  languages = a.get("languages")
-                  lMAO = ""
-                  for lmao in languages:
-                      lMAO += lmao + ",  "
+    search = m.text.split(None, 1)[1]
+    country = CountryInfo(search)
+    a = country.info()
+    name = a.get("name")
+    bb = a.get("altSpellings")
+    hu = ""
+    for p in bb:
+        hu += p + ",  "
 
+    area = a.get("area")
+    borders = ""
+    hell = a.get("borders")
+    for fk in hell:
+        borders += fk + ",  "
 
-caption = """
-**Country**: `{}`
-**Capital**: `{}`
+    call = ""
+    WhAt = a.get("callingCodes")
+    for what in WhAt:
+        call += what + "  "
+
+    capital = a.get("capital")
+    currencies = ""
+    fker = a.get("currencies")
+    for FKer in fker:
+        currencies += FKer + ",  "
+
+    HmM = a.get("demonym")
+    geo = a.get("geoJSON")
+    pablo = geo.get("features")
+    Pablo = pablo[0]
+    PAblo = Pablo.get("geometry")
+    EsCoBaR = PAblo.get("type")
+    iso = ""
+    iSo = a.get("ISO")
+    for hitler in iSo:
+        po = iSo.get(hitler)
+        iso += po + ",  "
+    fla = iSo.get("alpha2")
+    nox = fla.upper()
+    okie = flag.flag(nox)
+
+    languages = a.get("languages")
+    lMAO = ""
+    for lmao in languages:
+        lMAO += lmao + ",  "
+
+    nonive = a.get("nativeName")
+    waste = a.get("population")
+    reg = a.get("region")
+    sub = a.get("subregion")
+    tik = a.get("timezones")
+    tom = ""
+    for jerry in tik:
+        tom += jerry + ",   "
+
+    GOT = a.get("tld")
+    lanester = ""
+    for targaryen in GOT:
+        lanester += targaryen + ",   "
+
+    wiki = a.get("wiki")
+
+    caption = f"""<b><u>Information Gathered Successfully</b></u>
+<b>
+Country Name:- {name}
+Alternative Spellings:- {hu}
+Country Area:- {area} square kilometers
+Borders:- {borders}
+Calling Codes:- {call}
+Country's Capital:- {capital}
+Country's currency:- {currencies}
+Country's Flag:- {okie}
+Demonym:- {HmM}
+Country Type:- {EsCoBaR}
+ISO Names:- {iso}
+Languages:- {lMAO}
+Native Name:- {nonive}
+population:- {waste}
+Region:- {reg}
+Sub Region:- {sub}
+Time Zones:- {tom}
+Top Level Domain:- {lanester}
+wikipedia:- {wiki}</b>
+Gathered By @VegetaRobot ✨.</b>
+"""              
+   await m.reply_text(caption)
