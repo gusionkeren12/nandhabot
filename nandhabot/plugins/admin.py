3814a8a92c9220809516a72787ed4365c9130614
Aasf-Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2022-06-13 13:14:05 +0530

    Update admin.py

--- nandhabot/plugins/admin.py
+++ nandhabot/plugins/admin.py
@@ -1,4 +1,5 @@
 from pyrogram import filters
+from pyrogram.types import Message
 
 from nandhabot import bot
 
@@ -11,12 +12,12 @@ async def is_admins(chat_id: int):
 
 
 @bot.on_message(filters.command("ban"))
-async def ban(_, m):
+async def ban(_, m: Message):
     reply = m.reply_to_message
     user = m.from_user.id
     chat_id = m.chat.id
     if user not in (await is_admins(chat_id)):
-        return await message.reply_text("You are not admin")
+        return await message.reply_text("`You Are Not Admin`")
     if user in (await is_admins(chat_id)):
         await bot.ban_chat_member(m.chat.id, reply.from_user.id)
-        await m.reply_text(f"bammed! {reply.from_user.id}")
+        await m.reply_text(f"**Bammed!** {reply.from_user.id}")
