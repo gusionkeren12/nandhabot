6bc7dfcd36edf24c19bcd6fb98a51326425f2d72
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-14 00:27:59 +0530

    Update __main__.py

--- nandhabot/__main__.py
+++ nandhabot/__main__.py
@@ -15,38 +15,8 @@ logging.basicConfig(
     format=FORMAT,
     datefmt="[%X]",
 )
-logging.getLogger("pyrogram").setLevel(logging.INFO)
-
-async def start_bot():
-    restart_data = await clean_restart_stage()
-    x = await arq.wall("vegeta")
-    y = x.result
-    try:
-        print("Sending online status")
-        if restart_data:
-            await bot.edit_message_media(
-                restart_data["chat_id"],
-                restart_data["message_id"],
-                random.choice(y).url_image,
-                caption="**Saiyan Prince Vegeta Successfully Restarted With New Powers**",
-            )
-
-        else:
-           await bot.send_photo(f"@{SUPPORT_CHAT}", random.choice(y).url_image, caption="**Saiyan Prince Vegeta Was Successfully Deployed!**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                   [                  
-                       InlineKeyboardButton(
-                             text="[► Summon Me ◄]",
-                             url=f"https://t.me/VegetaRobot?startgroup=true")
-                     ] 
-                ]
-            ),
-        ) 
-    except Exception as e:
-        await bot.send_message("@VegetaSupport", e)
-        await idle
 
 if __name__ == "__main__":
    bot.run()
-   start_bot()
+     with bot:
+           bot.send_message("hello! There I'm  Alive")
