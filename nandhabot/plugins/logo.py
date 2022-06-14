e9e48a8cc388eeb3cb6d795147df5f0dd82b58cc
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-14 06:41:39 +0530

    Update logo.py

--- nandhabot/plugins/logo.py
+++ nandhabot/plugins/logo.py
@@ -7,6 +7,8 @@ from requests import get
 
 @bot.on_message(filters.command("logo"))
 async def logo(_, m):
+             if len(m.command) < 2:
+                return await m.reply("<b>Usage:</b>\n<code>/logo nandha</code>")
              text = m.text.split(None, 1)[1]
              msg = await m.reply_text("<b>Creating your logo</b>")
              logo = get((f"https://single-developers.up.railway.app/logo?name={text}").replace(' ','%20')).history[1].url
