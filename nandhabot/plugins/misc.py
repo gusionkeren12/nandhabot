484577fa550a2f5bc612b92b212726a946b038c5
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-14 15:05:32 +0530

    Update misc.py

--- nandhabot/plugins/misc.py
+++ nandhabot/plugins/misc.py
@@ -12,7 +12,7 @@ from urllib.parse import quote
 @bot.on_message(filters.command("spell"))
 async def spellcheck(_, m):
       if len(m.command) < 2:
-        return await message.reply_text("**usage**:\n`/spell boou`")
+        return await m.reply_text("**usage**:\n`/spell boou`")
       search = m.text.split(None, 1)[1]
       x = await arq.spellcheck(search)
       y = x.result
