02e988d4c8384c40bf683fb40180686136af6e48
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-14 07:59:36 +0530

    Update fun.py

--- nandhabot/plugins/fun.py
+++ nandhabot/plugins/fun.py
@@ -31,20 +31,20 @@ async def decrypt(_, m):
 async def truth(_, m):
          reply = m.reply_to_message
          if reply:
-               get = requests.get("https://api.truthordarebot.xyz/v1/truth").json
-               text = get["question"]
+               api = requests.get("https://api.truthordarebot.xyz/v1/truth").json
+               text = api["question"]
                await m.reply_text(text)
          else:
-               get = requests.get("https://api.truthordarebot.xyz/v1/truth").json
-               text = get["question"]
+               api = requests.get("https://api.truthordarebot.xyz/v1/truth").json
+               text = api["question"]
                await m.reply_text(text)
                       
 @bot.on_message(filters.command("dare"))
 async def dare(_, m):
          reply = m.reply_to_message
          if reply:
-               get = requests.get("https://api.truthordarebot.xyz/v1/dare").json
-               text = get["question"]
+               api = requests.get("https://api.truthordarebot.xyz/v1/dare").json
+               text = api["question"]
                dare = f"""
                Hey! {reply.from_user.mention}
                {m.from_user.mention} give you a dare!
@@ -52,8 +52,8 @@ async def dare(_, m):
                """
                await m.reply_text(dare)
          else:
-               get = requests.get("https://api.truthordarebot.xyz/v1/dare").json
-               text = get["question"]
+               api = requests.get("https://api.truthordarebot.xyz/v1/dare").json
+               text = api["question"]
                dare = f"""
                Hey! {m.from_user.mention} your dare here!
                <bDare</b>: <code>{text}</code>
