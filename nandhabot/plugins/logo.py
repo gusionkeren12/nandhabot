f998b1283504caab019e05ed61633d67ff243951
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-06-14 06:24:56 +0530

    Update logo.py

--- nandhabot/plugins/logo.py
+++ nandhabot/plugins/logo.py
@@ -7,6 +7,6 @@ import requests
 
 @bot.on_message(filters.command("logo"))
 async def logo(_, m):
-             text = message.text.split(None, 1)[1]
+             text = m.text.split(None, 1)[1]
              logo = get((f"https://single-developers.up.railway.app/logo?name={text}").replace(' ','%20')).history[1].url
              await m.reply_photo(logo,caption="Made by @VegetaRobot")
