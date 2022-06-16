33b96baea180b03ac9a7ddc5e0450131a5586b82
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-16 10:45:57 +0530

    Update __main__.py

--- nandhabot/__main__.py
+++ nandhabot/__main__.py
@@ -16,7 +16,7 @@ logging.basicConfig(
 
 
 if __name__ == "__main__":
-   tbot.start(bot_token=TOKEN)
+   tbot.start(bot_token=BOT_TOKEN)
    bot.run()
    with bot:
         bot.send_message(f"@{SUPPORT_CHAT}", "Hello there I'm Now online")
