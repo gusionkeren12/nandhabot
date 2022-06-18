cabb8bb59d22f03fc96235dc39c6eb80a75939da
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-18 10:41:20 +0530

    Update feedback.py

--- nandhabot/plugins/feedback.py
+++ nandhabot/plugins/feedback.py
@@ -8,7 +8,7 @@ from nandhabot.config import OWNER_ID
 async def feedback(_, m):
          if len(m.command) < 2:
             return await m.reply_text("gime a feedback!")
-         if m.from_user.id in OWNER_ID or dev_user:
+         if m.from_user.id in OWNER_ID:
              return await m.rely_text("hey! baka this is your bot!")
          text = m.text.split("", 1)
          user = m.from_user
