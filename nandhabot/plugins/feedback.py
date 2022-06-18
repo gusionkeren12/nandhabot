a1ac9c9d338deb42f2c727eff37234b804b0801e
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-18 12:00:06 +0530

    Update feedback.py

--- nandhabot/plugins/feedback.py
+++ nandhabot/plugins/feedback.py
@@ -8,14 +8,14 @@ from nandhabot.config import OWNER_ID
 @bot.on_message(filters.command(["feedback","bug"]))
 async def feedback(_, m):
          if len(m.command) < 2:
-            await m.reply_text("gime a feedback!")
-            return 
-         if m.from_user.id == OWNER_ID:
-               await m.reply_text("owner Baka!")
+               await m.reply_text("gime a feedback!")
                return 
          if m.chat.type == "private":
                 await m.reply_text("command work only groups")
                 return 
+         if m.from_user.id == OWNER_ID:
+               await m.reply_text("owner Baka!")
+               return 
          text = m.text.split(None, 1)[1]
          user = m.from_user
          chat = m.chat
@@ -23,9 +23,9 @@ async def feedback(_, m):
          datetimes = datetime.utcnow().strftime(datetimes_fmt)
          feedback = f""" **#NewFeedBack**
 FromChat: @{chat.username}
-user_id : {user.id}
-mention : {user.mention}
-from_msg : {datetimes}
+user_id: {user.id}
+mention: {user.mention}
+msg_date: {datetimes}
 Feedback: **{text}**
 """      
          await bot.send_message(f"@{SUPPORT_CHAT}", feedback)
