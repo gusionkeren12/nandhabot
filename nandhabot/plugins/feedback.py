095d6001626b84680a58ae976afbd9bfdaca89f5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-18 11:43:41 +0530

    Update feedback.py

--- nandhabot/plugins/feedback.py
+++ nandhabot/plugins/feedback.py
@@ -1,5 +1,6 @@
 from nandhabot import bot, dev_user, SUPPORT_CHAT
 from pyrogram import filters
+from datetime import datetime
 from nandhabot.config import OWNER_ID
 
 #made by t.me/nandhaxd
@@ -11,10 +12,13 @@ async def feedback(_, m):
          text = m.text.split(None, 1)[1]
          user = m.from_user
          chat = m.chat
+         datetimes_fmt = "%d-%m-%Y"
+         datetimes = datetime.utcnow().strftime(datetimes_fmt)
          feedback = f""" **#NewFeedBack**
 FromChat: @{chat.username}
 user_id : {user.id}
 mention : {user.mention}
+from_msg : {datetimes}
 Feedback: **{text}**
 """      
          await bot.send_message(f"@{SUPPORT_CHAT}", feedback)
