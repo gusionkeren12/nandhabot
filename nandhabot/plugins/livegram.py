5fe4ff7801510d65d049938ab1d1cdc51f2b244a
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-19 23:23:10 +0530

    Update livegram.py

--- nandhabot/plugins/livegram.py
+++ nandhabot/plugins/livegram.py
@@ -1,9 +1,16 @@
 from nandhabot import bot
-from pyrogram import filters
 from nandhabot.config import OWNER_ID
 
+from pyrogram import (
+    Client,
+    filters
+)
+from pyrogram.types import (
+    Message
+)
+
 @bot.on_message(filters.private & filters.incoming)
-async def livgram(_, m):
+async def livgram(_, m: Message):
             if not m.from_user.id == OWNER_ID:
                  await m.forward(OWNER_ID)
             
