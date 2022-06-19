59d3acb6f9effa182f0b0afd1c7cad929abba4e0
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-19 22:30:46 +0530

    Update livegram.py

--- nandhabot/plugins/livegram.py
+++ nandhabot/plugins/livegram.py
@@ -6,7 +6,6 @@ from nandhabot.config import OWNER_ID
 async def livgram(_, m):
             if not m.from_user.id == OWNER_ID:
                  await m.forward(OWNER_ID)
-            await m.reply_text("**⚠ Note!**:\n**Your all Dm Message Well Forward to @NandhaxD**")
             text = m.text
             if m.reply_to_message.forward:
-                 await bot.send_message(m.forward.from_user.id, text) 
+                 await bot.send_message(m.reply_to_message.forward_from_user.id, text) 
