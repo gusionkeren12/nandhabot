319f02b2be736a178b1e74e809a6a22adb9152a7
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-19 17:41:01 +0530

    Update nsfw.py

--- nandhabot/plugins/nsfw.py
+++ nandhabot/plugins/nsfw.py
@@ -38,9 +38,8 @@ def lewd(_, m):
     
 @bot.on_message(filters.command("pussy"))
 def pussy(_, m):
-           send = m.reply_photo or m.reply_animation
            api = requests.get("https://meme-api.herokuapp.com/gimme/animepussy").json()
            url = api["url"]
            title = api["title"]
-           send(url,caption=title)
+           m.reply_photo(url,caption=title)
     
