d234ba088dd0f4615415cd60e32b40511b80c15e
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-20 22:24:15 +0530

    Update core.py

--- nandhabot/plugins/core.py
+++ nandhabot/plugins/core.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from nandhabot import dev_user as DEV_USERS
 from nandhabot import TEMP_DOWNLOAD_DIRECTORY as path
 from datetime import datetime
-water = './nandhabot/images/IMG_20220522_180127_083.jpg'
+water = "https://telegra.ph/file/a5005e20487831c9ad219.jpg"
 client = tbot
 
 @register(pattern=r"^/pyupload ?(.*)")
