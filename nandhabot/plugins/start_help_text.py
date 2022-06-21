a2d113169987309c5acaa652db6b9486c5c92be5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-22 01:08:43 +0530

    Update start_help_text.py

--- nandhabot/plugins/start_help_text.py
+++ nandhabot/plugins/start_help_text.py
@@ -41,7 +41,7 @@ async def alive(_, m: Message):
     pm_caption += f"**♡ Telethon Version :** `{telethon_version}`\n\n"
     pm_caption += f"**♡ Pyrogram Version :** `{pyrogram_version}`\n\n"
     pm_caption += "**♡ My Master :** [Nandha](https://t.me/nandhaxd) "
-    await msg.edit_text(text=(pm_caption)
+    await msg.edit_text(text=(pm_caption))
 
            
 BOT_IMG = [ "https://telegra.ph/file/b3fbf990e0b67ede241a3.jpg",
