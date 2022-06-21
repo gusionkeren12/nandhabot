fa9c7f57fe6db2d74107a129df6559be2cbd07f9
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 20:53:42 +0530

    Create admin.py

---
+++ nandhabot/plugins/admin.py
@@ -0,0 +1,8 @@
+from pyrogram import filters 
+from nandhabot import bot
+
+
+def is_admin(group_id: int, user_id: int):
+    try:
+        user_data = bot.get_chat_member(group_id, user_id)
+        if user_data.status == 'administrator' or user_data.status == 'creator':
