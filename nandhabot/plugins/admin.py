560ecc4faa67ad59d669ce8f4c104df96045bb41
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 23:34:20 +0530

    Update admin.py

--- nandhabot/plugins/admin.py
+++ nandhabot/plugins/admin.py
@@ -1,5 +1,15 @@
 from nandhabot import bot
 from pyrogram import filters
+from nandhabot.utils.admin_check import *
+
+async def admin_filter_f(filt, client, message):
+    return await admin_check(message)
+
+
+admin_fliter = filters.create(
+    func=admin_filter_f,
+    name="AdminFilter"
+)
 
 @bot.on_message(filters.command("ban"))
 async def ban(_, m):
