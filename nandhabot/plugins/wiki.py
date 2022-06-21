aafb93d8274cf764ddebdcacfc317e2abb32982f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 19:04:53 +0530

    Update wiki.py

--- nandhabot/plugins/wiki.py
+++ nandhabot/plugins/wiki.py
@@ -7,6 +7,9 @@ from nandhabot import bot
 @bot.on_message(filters.command(["wiki", "Wikipedia"]))
 async def wikipediasearch(_, message: Message):
     reply = message.reply_to_message
+    if len(message.command) < 2:
+          await message.reply_text("example:\n`/wiki telegram`")
+          return 
     query =  message.text.split(None, 1)[1] 
     if not query:
         await message.reply_text("Invalid Syntax see help menu to know how to use this command")
