ef357d6a67e2d70afb0f4d8962fe9496adea6a66
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-21 20:54:32 +0530

    Update feedback.py
    filters.Group

--- nandhabot/plugins/feedback.py
+++ nandhabot/plugins/feedback.py
@@ -12,7 +12,7 @@ vegeta_img = [ "https://telegra.ph/file/03ba8fea3c3ed2b98b68a.jpg",
 "https://telegra.ph/file/51323082ef6051f3a9721.jpg",
 "https://telegra.ph/file/072bc7f5f9fdf7f04acb3.jpg"]
 
-@bot.on_message(filters.groups & filters.command(["feedback","bug"]))
+@bot.on_message(filters.group & filters.command(["feedback","bug"]))
 async def feedback(_, m):
          if len(m.command) < 2:
                await m.reply_text("**Gime a Feedback!**")
