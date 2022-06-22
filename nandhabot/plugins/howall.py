8a1757cf7b34b61db68ee15d0642ffa648cc11ff
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-22 16:29:57 +0530

    Update howall.py

--- nandhabot/plugins/howall.py
+++ nandhabot/plugins/howall.py
@@ -17,13 +17,13 @@ async def wish(_, m):
                        text = m.text.split(None, 1)[1]
                        wish_text = f"Hey Dear {m.from_user.mention}"
                        wish_text += f" Your Wish: {text}"
-                       wish_text += "Possible to Your Wish Cost: {wish_count} 🌠"
+                       wish_text += f"Possible to Your Wish Cost: {wish_count} 🌠"
                        await m.reply_text(text=(wish_text))
             if reply:
                   text = reply.text
                   wish_text = f"Hey Dear {m.from_user.mention}"
                   wish_text += f" Your Wish: {text}"
-                  wish_text += "Possible to Your Wish Cost: {wish_count} 🌠"
+                  wish_text += f"Possible to Your Wish Cost: {wish_count} 🌠"
                   await m.reply_text(text=(wish_text))
 
 BUTTON = [[Button.url("❓ What Is This", "https://t.me/vegetaUpdates/173")]]
