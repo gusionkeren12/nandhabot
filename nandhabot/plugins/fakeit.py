90a35fa9828b1929257ec644f38e18bb102dec73
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-22 08:57:15 +0530

    Update fakeit.py

--- nandhabot/plugins/fakeit.py
+++ nandhabot/plugins/fakeit.py
@@ -18,5 +18,5 @@ async def fakeit(_, m):
         job = fake.job()
         android = fake.android_platform_token()
         pc = fake.chrome()
-        await message.reply_text(
+        await m.reply_text(
         f"<b><u> Fake Information Generated:</b></u>\n<b>Name :-</b><code>{name}</code>\n\n<b>Address:-</b><code>{address}</code>\n\n<b>IP ADDRESS:-</b><code>{ip}</code>\n\n<b>credit card:-</b><code>{cc}</code>\n\n<b>Email Id:-</b><code>{email}</code>\n\n<b>Job:-</b><code>{job}</code>\n\n<b>android user agent:-</b><code>{android}</code>\n\n<b>Pc user agent:-</b><code>{pc}</code>")
