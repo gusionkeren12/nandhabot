f8454588925f2285b725c61243dd7287d788ce4e
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-23 10:26:55 +0530

    Update tools.py

--- nandhabot/plugins/tools.py
+++ nandhabot/plugins/tools.py
@@ -128,7 +128,7 @@ async def okgoogle(img):
             pass
         BUTTON = [[Button.url("image src", url=f"{fetchUrl}"),
                                   Button.url("google src", url=f"{imgspage}")]]
-        await dev.edit("**below click the buttons**!",BUTTON
+        await img.reply("**below click the buttons**!",BUTTON
         )
 
 
