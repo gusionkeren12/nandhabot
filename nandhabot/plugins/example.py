14ef7fa044bf6b7a60604c73e3f76fc8443c7681
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-25 21:11:21 +0530

    Update example.py

--- nandhabot/plugins/example.py
+++ nandhabot/plugins/example.py
@@ -1,1 +1,13 @@
 
+
+
+def promote(update: Update, context):
+    message = update.effective_message
+    chat = update.effective_chat
+    user = update.effective_user
+
+    user_member = chat.get_member(user.id)
+    if not user_member.status == 'administrator' or user_member.status == 'creator':
+            message.reply_text("Your not admin")
+    if user_member.status == 'administrator' or user_member.status == 'creator':
+             message.reply_text("yes you can")
