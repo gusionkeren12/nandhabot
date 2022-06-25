13a2da6fa2285efcbe66253052b8b6196555825e
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-25 17:17:27 +0530

    Update example.py

--- nandhabot/plugins/example.py
+++ nandhabot/plugins/example.py
@@ -8,8 +8,7 @@ def ban(update, context):
       chat = update.effective_chat
       message = chat = update.effective_message
       bot = context.bot
-      user = update.effective_user
-      user_id = bot.get_chat(user).id
+      user_id = message.reply_to_message.from_user.id
       user_member = chat.get_member(user_id)
       if not user_member.status == 'administrator' or user_member.status == 'creator':
           message.reply_text("your not admin")
@@ -17,6 +16,7 @@ def ban(update, context):
       if user_member.status == 'administrator' or user_member.status == 'creator':
           chat.kick_member(user_id)
           message.reply_text("banned!")
+            
 
 ban_cmd = CommandHandler("ban", ban)
 dispatcher.add_handler(ban_cmd)
