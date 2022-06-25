a71dd26122a9b3c553b30966edb73808cac3db57
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-25 18:35:52 +0530

    Create bans.py

---
+++ nandhabot/plugins/bans.py
@@ -0,0 +1,18 @@
+from telegram import Update
+from nandhabot import dispatcher
+from telegram.ext import CallbackContext, CommandHadler
+
+def ban(update: Update, context: CallbackContext):
+          message = update.effective_message
+          chat = update.effective_chat
+          member = update.effective_chat.get_member
+          if not member(update.effective_user.id).status == ("administrator" or "creator"):
+                    message.reply_text("your not admin")
+          if member(update.effective_user.id).status == ("administrator" or "creator"):
+                       chat.ban_member(message.reply_to_message.from_user.id)
+                       message.reply_text("banned!")
+
+
+ban_cmd = CommandHandler("ban", ban) 
+
+dispatcher.add_handler(ban_cmd)
