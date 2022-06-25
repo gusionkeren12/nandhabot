caebee3ab353c5a9d8177e3fe466198482a3499f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-25 23:16:52 +0530

    Update example.py

--- nandhabot/plugins/example.py
+++ nandhabot/plugins/example.py
@@ -6,11 +6,17 @@ def ban(update: Update, context):
     message = update.effective_message
     chat = update.effective_chat
     user = update.effective_user
-
+    TEXT= f"""❕ EVENT BANNED:
+┏━━━━━━━━━━━━━━━━┓
+┃ ➢ : {chat.username}
+┃➢ : [{message.from_user.first_name}](tg://user?id={message.from_user.id})
+┃➢ : [{message.reply_to_message.from_user.first_name}](tg://user?id={message.reply_to_message.from_user.id}]
+┗━━━━━━━━━━━━━━━━┛
+"""
     user_member = chat.get_member(user.id)
     if user_member.status == 'administrator' or user_member.status == 'creator':
              chat.ban_member(message.reply_to_message.from_user.id)
-             message.reply_text("yes you can\banned!")
+             message.reply_text(TEXT)
     else:
              message.reply_text("your not admin")
 
