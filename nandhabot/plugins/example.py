e06004bc84661128bfe8b5492104fafe9397c3e3
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-25 23:24:02 +0530

    Update example.py

--- nandhabot/plugins/example.py
+++ nandhabot/plugins/example.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, ParseMode
 from telegram.ext import CommandHandler,run_async
 from nandhabot import dispatcher
 
@@ -6,17 +6,17 @@ def ban(update: Update, context):
     message = update.effective_message
     chat = update.effective_chat
     user = update.effective_user
-    TEXT= f"""❕ EVENT BANNED:
+    TEXT= f"""❕* EVENT BANNED:*
 ┏━━━━━━━━━━━━━━━━┓
-┃ ➢ : {chat.username}
-┃➢ : [{message.from_user.first_name}](tg://user?id={message.from_user.id})
-┃➢ : [{message.reply_to_message.from_user.first_name}](tg://user?id={message.reply_to_message.from_user.id}]
+┃ ➢ : @{chat.username}
+┃➢ : [Admin](tg://user?id={message.from_user.id})
+┃➢ : [User](tg://user?id={message.reply_to_message.from_user.id}]
 ┗━━━━━━━━━━━━━━━━┛
 """
     user_member = chat.get_member(user.id)
     if user_member.status == 'administrator' or user_member.status == 'creator':
              chat.ban_member(message.reply_to_message.from_user.id)
-             message.reply_text(TEXT)
+             message.reply_text(TEXT,parse_mode=ParseMode.MARKDOWN)
     else:
              message.reply_text("your not admin")
 
