303dd140cab5dc6329e7878b2c391d41b5064a3d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-28 00:45:23 +0530

    Update pyroadmin.py

--- nandhabot/plugins/pyroadmin.py
+++ nandhabot/plugins/pyroadmin.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from nandhabot import bot
 
-@bot.on_message(filters.command("pdel"))
-async def pdel(_, m):
+@bot.on_message(filters.command("del"))
+async def del(_, m):
      reply = m.reply_to_message
      chat = m.chat
      user = m.from_user
@@ -20,3 +20,24 @@ async def pdel(_, m):
                await reply.delete()
                await m.reply_text("deleted!")
       
+@bot.on_message(filters.command("ban"))
+async def del(_, m):
+     reply = m.reply_to_message
+     chat = m.chat
+     user = m.from_user
+     user_stats = await bot.get_chat_member(chat.id, user.id)
+     if not user_stats.privileges:
+            await m.reply_text("Your not admin")
+            return 
+     if not reply:
+             await m.reply_text("reply to msg for deleting")
+             return 
+     if not user_stats.privileges.can_restrict_members:
+               await m.reply_text("**your don't having the permission of**:\n`can_restrict_members`")
+               return 
+     if user_stats.privileges.can_restrict_members:
+             ban = reply.from_user or reply.sender_chat
+             if ban:
+                    await bot.ban_chat_member(chat.id, ban.id)
+                     await m.reply_text("banned!")
+                    
