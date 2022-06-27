ee5a56dc4178c5466d0d9cd267b4eb85850a11f1
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-28 00:01:55 +0530

    Update pyroadmin.py

--- nandhabot/plugins/pyroadmin.py
+++ nandhabot/plugins/pyroadmin.py
@@ -7,12 +7,12 @@ async def pdel(_, m):
      chat = m.chat
      user = m.from_user
      user_stats = await bot.get_chat_member(chat.id, user.id)
-     if user_stats.privileges.can_delete_messages == "false":
+     if not user_stats.privileges::
             await message.reply_text("You don't having a deleting permission")
             return 
      if not reply:
              await message.reply_text("reply to msg for deleting")
              return 
-     if user_stats.privileges.can_delete_messages == "true":
+     if user_stats.privileges.can_delete_messages:
                await reply.delete()
                await message.reply_text("deleted!")
