731340bd626964e2a7b7e8a6f0b45d0ff48dd173
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-27 23:55:14 +0530

    Update pyroadmin.py

--- nandhabot/plugins/pyroadmin.py
+++ nandhabot/plugins/pyroadmin.py
@@ -2,14 +2,17 @@ from pyrogram import filters
 from nandhabot import bot
 
 @bot.on_message(filters.command("pdel"))
-async def (_, m):
+async def pdel(_, m):
      reply = m.reply_to_message
      chat = m.chat
      user = m.from_user
      user_stats = await bot.get_chat_member(chat.id, user.id)
-     it user_stats.privileges.can_delete_messages == "false":
+     if user_stats.privileges.can_delete_messages == "false":
             await message.reply_text("You don't having a deleting permission")
             return 
-     it user_stats.privileges.can_delete_messages == "true":
+     if not reply:
+             await message.reply_text("reply to msg for deleting")
+             return 
+     if user_stats.privileges.can_delete_messages == "true":
                await reply.delete()
                await message.reply_text("deleted!")
