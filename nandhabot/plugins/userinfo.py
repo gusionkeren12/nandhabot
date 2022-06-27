cdcd4f68b97bcfcf66a8d7cbb7d70e13456b8816
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-27 09:27:26 +0530

    Update userinfo.py

--- nandhabot/plugins/userinfo.py
+++ nandhabot/plugins/userinfo.py
@@ -5,46 +5,6 @@ from pyrogram.types import Message
 from nandhabot import dev_user, bot
 from nandhabot.utils.sections import section
 
-C_INFO = """
-"✪ ID": user_id,
-"✪ Name": [first_name],
-"✪ Username": [("@" + username) if username else "Null"],
-        "✪ Mention": [mention],
-"""
-@bot.on_message(filters.command("cinfo"))
-async def cinfo(_, m):
-       reply = m.reply_to_message
-       if not reply:
-            await m.reply_text("yoo! ultra noob reply to channel")
-            return 
-       if not reply.sender_chat:
-            await m.reply_text("yoo! ultra noob reply to channel")
-            return 
-       if reply.sender_chat:
-             message = await m.reply_text("information gathering!!!")
-             id = reply.sender_chat.id
-             type = reply.sender_chat.type
-             name = reply.sender_chat.title
-             username = reply.sender_chat.username
-             pfp = reply.sender_chat.photo
-       if not pfp:
-            text = f"✪ TYPE: {type}\n\n"
-            text += f"✪ ID: {id}\n\n"
-            text += f"✪ NAME: {name}\n\n"
-            text += f"✪ USERNAME: @{username}\n\n"
-            text += f"✪ MENTION: [link](t.me/{username})"
-            await m.reply_text(text)
-            return 
-       image = reply.sender_chat.photo
-       if image:
-            photo = await bot.download_media(image.big_file_id)
-            text = f"✪ TYPE: {type}\n\n"
-            text += f"✪ ID: {id}\n\n"
-            text += f"✪ NAME: {name}\n\n"
-            text += f"✪ USERNAME: @{username}\n\n"
-            text += f"✪ MENTION: [link](t.me/{username})"
-            await m.reply_photo(photo,caption=(text))
-            await message.delete()
             
 async def get_user_info(user, already=False):
     if not already:
