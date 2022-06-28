faddf1369664e068fceab4c8860501b17ce7331b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-28 19:53:02 +0530

    Update pyroadmin.py

--- nandhabot/plugins/pyroadmin.py
+++ nandhabot/plugins/pyroadmin.py
@@ -111,6 +111,6 @@ async def setgrouptitle(_, m):
                return 
      if user_stats.privileges.can_manage_chat:
                photo = await bot.download_media(file)
-               await bot.set_chat_photo(photo)
+               await bot.set_chat_photo(chat.id, photo)
                await m.reply_text("*Successfully group new photo changed!")
 
