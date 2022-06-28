77f93752be6bca73d59b74a440b2c9a172a0df3e
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-28 10:49:39 +0530

    Update pyroadmin.py

--- nandhabot/plugins/pyroadmin.py
+++ nandhabot/plugins/pyroadmin.py
@@ -7,18 +7,22 @@ async def delete(_, m):
      chat = m.chat
      user = m.from_user
      user_stats = await bot.get_chat_member(chat.id, user.id)
+     bot_stats = await bot.get_chat_member(chat.id, "self")
      if not user_stats.privileges:
             await m.reply_text("Your not admin")
             return 
      if not reply:
              await m.reply_text("reply to msg for deleting")
              return 
+     If not bot_stats.privileges.can_delete_messages:
+               await m.reply_text("**your are missing the permission of**:\n`can_delete_messages`")
+               return 
      if not user_stats.privileges.can_delete_messages:
-               await m.reply_text("**your don't having the permission of**:\n`can_delete_messages`")
+               await m.reply_text("**your are missing the permission of**:\n`can_delete_messages`")
                return 
      if user_stats.privileges.can_delete_messages:
                await reply.delete()
-               await m.reply_text("deleted!")
+               await m.reply_text("replied message deleted!")
       
 @bot.on_message(filters.command("ban"))
 async def banned(_, m):
