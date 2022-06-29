2c80ffaabd611cb013ff92ebbc4235940a0603dc
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-29 17:41:30 +0530

    Update paste.py

--- nandhabot/plugins/paste.py
+++ nandhabot/plugins/paste.py
@@ -14,25 +14,6 @@ def spacebin(text):
     return f"https://spaceb.in/{res.json()['payload']['id']}"
 
 
-
-@bot.on_message(filters.command('paste'))
-def paste(_, m):
-    reply = m.reply_to_message
-    if not reply:
-           text = m.text.split(None, 1)[1]
-           spacebin_url = spacebin(text)
-           m.reply_text(spacebin_url,disable_web_page_preview=True)
-           return 
-    text = reply.text or reply.caption
-    if reply:
-        spacebin_url = spacebin(text)
-        caption = f"[SPACEBIN]({spacebin_url})"
-        m.reply(text=caption,
-                      reply_markup=InlineKeyboardMarkup(
-                          [[InlineKeyboardButton("SPACEBIN", url=spacebin_url)]]),disable_web_page_preview=True)
-
-
-
 def _netcat(host, port, content):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
@@ -45,15 +26,32 @@ def _netcat(host, port, content):
         return data
     s.close()
     
-async def devpaste(content):
+async def ezup(content):
     loop = get_running_loop()
     link = await loop.run_in_executor(
         None, partial(_netcat, "ezup.dev", 9999, content)
     )
     return link
 
-@bot.on_message(filters.command("test"))
-async def test(_, m):
-    text = m.reply_to_message.text
-    link = await devpaste(text)
-    await m.reply_text(link)
+@bot.on_message(filters.command('paste'))
+def paste(_, m):
+    reply = m.reply_to_message
+    if not reply:
+           text = m.text.split(None, 1)[1]
+           spacebin_url = spacebin(text)
+           link = await ezup(text)
+           m.reply_text(f"{link} +\n{spacebin_url}",disable_web_page_preview=True)
+           return 
+    text = reply.text or reply.caption
+    if reply:
+        spacebin_url = spacebin(text)
+        link = await ezup(text)
+        caption = f"[SPACEBIN]({spacebin_url}) | [ezup.dev]({link})"
+        m.reply(text=caption,
+                      reply_markup=InlineKeyboardMarkup(
+                          [[InlineKeyboardButton("SPACEBIN", url=spacebin_url),
+                           ],[ InlineKeyboardButton("EZUP.DEV", url=link)]]),disable_web_page_preview=True)
+
+
+
+
