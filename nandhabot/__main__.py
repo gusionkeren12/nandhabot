19419b0aa4e286c7f22bd9a0285977d016b7e924
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-04 22:18:45 +0530

    Update __main__.py

--- nandhabot/__main__.py
+++ nandhabot/__main__.py
@@ -9,7 +9,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 if __name__ == "__main__":
    tbot.start(bot_token=BOT_TOKEN)
    updater.start_polling()
-   ubot.start()
    bot.run()
    with bot:
         bot.send_message(f"@{SUPPORT_CHAT}", "Hello there I'm Now online")
