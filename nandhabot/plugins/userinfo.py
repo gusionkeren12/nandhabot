597d1456d8d7bf4ab49cb050170e4ef6fad94a0a
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-07-05 22:58:11 +0530

    Update userinfo.py

--- nandhabot/plugins/userinfo.py
+++ nandhabot/plugins/userinfo.py
@@ -5,42 +5,35 @@ from pyrogram.types import Message
 from nandhabot import dev_user, bot
 from nandhabot.utils.sections import section
 
-
 @bot.on_message(filters.command("cinfo"))
-async def cinfo(_, m):
-       reply = m.reply_to_message
-       if not reply:
-            await m.reply_text("yoo! ultra noob reply to channel")
-            return 
-       if not reply.sender_chat:
-            await m.reply_text("yoo! ultra noob reply to channel")
-            return 
-       if reply.sender_chat:
-             message = await m.reply_text("information gathering!!!")
-             id = reply.sender_chat.id
-             type = reply.sender_chat.type
-             name = reply.sender_chat.title
-             username = reply.sender_chat.username
-             pfp = reply.sender_chat.photo
-       if not pfp:
-            text = f"✪ **TYPE:** Channel\n\n"
-            text += f"✪ **ID:** {id}\n\n"
-            text += f"✪ **NAME:** {name}\n\n"
-            text += f"✪ **USERNAME:** @{username}\n\n"
-            text += f"✪ **MENTION:** [link](t.me/{username})"
-            await m.reply_text(text)
-            await message.delete()
-            return 
-       image = reply.sender_chat.photo
-       if image:
-            photo = await bot.download_media(image.big_file_id)
-            text = f"✪ **TYPE:** Channel\n\n"
-            text += f"✪ **ID:** {id}\n\n"
-            text += f"✪ **NAME:** {name}\n\n"
-            text += f"✪ **USERNAME:** @{username}\n\n"
-            text += f"✪ **MENTION:** [link](t.me/{username})"
-            await m.reply_photo(photo,caption=(text))
-            await message.delete()
+async def cinfo(_, message):
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user.id
+    elif not message.reply_to_message and len(message.command) == 1:
+        user = message.from_user.id
+    elif not message.reply_to_message and len(message.command) != 1:
+        user = message.text.split(None, 1)[1]
+    info = await bot.get_chat(user)
+    c_info = ""
+    id = info.id
+    name = info.title
+    username = info.username
+    photo = info.photo.big_file_id
+    msg = message.reply_text("ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴘʀᴏᴄᴇssɪɴɢ...")
+    if info.photo:
+        channel_photo = await bot.download_media(photo,first_name=id)
+        await message.reply_photo(channel_photo,caption = (
+                       f"ɪᴅ: `{id}`"
+                       f"ɴᴀᴍᴇ: {name}"
+                       f"ᴜsᴇʀɴᴀᴍᴇ: @{username}" ))
+    elif not info.photo:
+             await message.reply_text(text = (
+                       f"ɪᴅ: `{id}`"
+                       f"ɴᴀᴍᴇ: {name}"
+                       f"ᴜsᴇʀɴᴀᴍᴇ: @{username}" ))
+    await msg.delete()
+
+
             
 async def get_user_info(user, already=False):
     if not already:
@@ -108,7 +101,7 @@ async def info_func(_, message: Message):
     elif not message.reply_to_message and len(message.command) != 1:
         user = message.text.split(None, 1)[1]
 
-    m = await message.reply_text("Information Processing...")
+    m = await message.reply_text("Information Processing..")
 
     try:
         info_caption, photo_id = await get_user_info(user)
