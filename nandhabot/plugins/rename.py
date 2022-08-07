b3487d552cd6d6bd9ce91c5b8f17e5095c60fe22
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-07 11:01:11 +0530

    Update rename.py

--- nandhabot/plugins/rename.py
+++ nandhabot/plugins/rename.py
@@ -2,7 +2,7 @@ from nandhabot import bot
 import os
 from nandhabot.utils.sendlog import send_log
 from pyrogram import filters
-from pyrogram.types import inlineKeybordMarkup,  inlineKeybord
+from pyrogram.types import InlineKeybordMarkup,  InlineKeybordButton
 
 
 @bot.on_message(filters.command('rename'))
@@ -18,13 +18,14 @@ def rename(_, message):
             buttons = [[InlineKeyboardButton('FILE' , callback_data="videotype"),
                                 InlineKeybordButton('VIDEO',  callback_data='filetype')]] 
         x.edit("Choose the below Button Which Type You Want!",
-        reply_markup=inlineKeybordMarkup(buttons)
+        reply_markup=inlineKeybordMarkup(buttons))
         
 
 
 @bot.on_message(filters.regex("filetype"))
-async def filetype(_, m)
+def filetype(_, m):
         x = message.reply_text("Downloading.....")
         path = reply.download(file_name=filename)
+        x.edit("uploading now..... ")
         message.reply_document(path)
         os.remove(path)
