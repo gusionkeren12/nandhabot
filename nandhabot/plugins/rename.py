2d2a1988d0df15ecc16cc4afabbc80b389a98160
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-13 14:58:10 +0530

    Update rename.py

--- nandhabot/plugins/rename.py
+++ nandhabot/plugins/rename.py
@@ -8,13 +8,14 @@ from pyrogram.types import CallbackQuery
 
 @bot.on_message(filters.command('rename'))
 def rename(_, message):
-    global reply, filename
+    global reply, filename, user_id
     try:
         filename = message.text.replace(message.text.split(" ")[0], "")
 
     except Exception as e:
         message.reply_text(e)
     reply = message.reply_to_message
+    user_id = message.from_user.id
     if reply:
             buttons = [[ 
                                 InlineKeyboardButton("FILE" , callback_data="filetype"),
@@ -28,11 +29,14 @@ def rename(_, message):
 
 @bot.on_callback_query(filters.regex("filetype"))
 def filtypes(_, query: CallbackQuery):
-        dl = reply.download(file_name=filename)
-        x = query.message.reply_text("uploading now...")
-        query.message.reply_document(dl)
-        x.edit(f"**Successfully Uploaded:**\n**xD** {query.message.reply_to_message.from_user.mention}")
-     
+        if query.message.from_user.id in = user_id:
+           dl = reply.download(file_name=filename)
+           x = query.message.reply_text("uploading now...")
+           query.message.reply_document(dl)
+           x.edit(f"**Successfully Uploaded:**\n**xD** {query.message.reply_to_message.from_user.mention}")
+
+        else:
+                query.answer("this is not your requested")
 
 @bot.on_callback_query(filters.regex("videotype"))
 def videotypes(_, query: CallbackQuery):
