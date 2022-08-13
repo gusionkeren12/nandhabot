77b63d66d62442525b7ec3a45547b58670e77bc8
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-13 16:39:27 +0530

    Update save.py

--- nandhabot/plugins/save.py
+++ nandhabot/plugins/save.py
@@ -3,7 +3,9 @@ from nandhabot import bot
 
 @bot.on_message(filters.command("save") & filters.user(1491497760))
 async def save(_, m):
-            reply = m.reply_to_message
-            await bot.copy_message("@Nandha_days", m.chat.id, reply.id)
-            await m.reply_text("**Successfully Saved!**\n**@Nandha_Days**")
-            
+            try:
+                reply = m.reply_to_message
+               await bot.copy_message("@Nandha_days", m.chat.id, reply.id)
+               await m.reply_text("**Successfully Saved!**\n**@Nandha_Days**")
+            except Exception as e:
+                  await m.reply_text(f"error: {e}")
