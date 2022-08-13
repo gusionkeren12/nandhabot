d8aea8e1b470199a7f59aa055f2b7256f771f905
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-13 17:24:49 +0530

    Update paste.py

--- nandhabot/plugins/paste.py
+++ nandhabot/plugins/paste.py
@@ -62,7 +62,6 @@ async def paste(_, m):
           await m.reply_text(text=caption,
                       reply_markup=InlineKeyboardMarkup(
                           [[InlineKeyboardButton("SPACEBIN", url=spacebin_url),
-                           ],[InlineKeyboardButton("HASTEBIN", url=url),
                            ],[ InlineKeyboardButton("EZUP.DEV", url=link)]]),disable_web_page_preview=True)
     
         
