3121745dc7f1c96131b1dacbed77789c4046eaca
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-15 08:58:30 +0530

    Update feedback.py

--- nandhabot/plugins/feedback.py
+++ nandhabot/plugins/feedback.py
@@ -55,7 +55,7 @@ Feedback: **{text}**
 
 @bot.on_callback_query(filters.regex("refeed"))
 async def replyfeedback(_, query: CallbackQuery):
-          msg = await query.message.relpy_text("Give me a text to reply feed-user")
+          await bot.send_message(query.message.chat.id. "Give me a text to reply feed-user")
           await bot.send_message(user.id, "ok")
                                 
   
