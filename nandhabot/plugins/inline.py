7b8f4674b300de6c3215bcae810575a175afb053
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-21 10:43:47 +0530

    Update inline.py

--- nandhabot/plugins/inline.py
+++ nandhabot/plugins/inline.py
@@ -61,7 +61,7 @@ Thanks for using and keep support my channels!""",
             results=[
                 InlineQueryResultArticle(
                     input_message_content=InputTextMessageContent(
-                    text=""" ⚠️** Warning**: Urban Dictionary does not always provide accurate descriptions
+                    text=f""" ⚠️** Warning**: Urban Dictionary does not always provide accurate descriptions
                     
                     • **Word**: {ud_text}
                     
