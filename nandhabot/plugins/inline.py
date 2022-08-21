3eb1308b9d31edf00463826f83ef903d0628409d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-21 11:04:14 +0530

    Update inline.py

--- nandhabot/plugins/inline.py
+++ nandhabot/plugins/inline.py
@@ -61,8 +61,8 @@ Thanks for using and keep support my channels!""",
             results=[
                 InlineQueryResultArticle(
                     input_message_content=InputTextMessageContent(
-                    f"⚠️** Warning**: Urban Dictionary does not always provide accurate descriptions\n• **Word**: {ud_text}\n• **Meaning**: {results["list"][0]["definition"]}\n• **Example**: {results["list"][0]["example"]}\n")
-                    ,thumb_url="https://telegra.ph/file/fab6e21499ac634c02e00.jpg",
+                    f"""⚠️** Warning**: Urban Dictionary does not always provide accurate descriptions\n• **Word**: {ud_text}\n• **Meaning**: {results["list"][0]["definition"]}\n• **Example**: {results["list"][0]["example"]}\n
+                    """),thumb_url="https://telegra.ph/file/fab6e21499ac634c02e00.jpg",
                     title=f"🔥 How horny are U?",
                     description=f"Send Your Current hornyess To This Chat.")])
                     
