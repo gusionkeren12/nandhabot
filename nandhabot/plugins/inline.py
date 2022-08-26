bda523bb5e1e9b6506dbfe9cf6b064c61b6df8ae
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-26 20:21:21 +0530

    Update inline.py

--- nandhabot/plugins/inline.py
+++ nandhabot/plugins/inline.py
@@ -62,7 +62,7 @@ Thanks for using and keep support my channels!""",
         switch_pm_parameter="inline",
                 )
         text = string.split(None, 1)[1].strip()
-        x = await arq.wall(text)
+        y = await arq.wall(text)
         for y in x.result:
           image = y.url_image
         await client.answer_inline_query(
