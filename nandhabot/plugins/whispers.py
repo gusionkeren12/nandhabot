67a0b68a2701f3f0e4933a24e2c84624080c9552
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-27 21:40:32 +0530

    Update whispers.py

--- nandhabot/plugins/whispers.py
+++ nandhabot/plugins/whispers.py
@@ -17,9 +17,9 @@ async def whisper(_, message):
       user_id = message.text.split(" ")[1]
       text = message.text.split(" ")[2]
       bot_stats = await bot.get_chat_member(message.chat.id, "self")
-      if bot_stats.privileges:
-           await message.delete()
-           return 
+         if bot_stats.privileges:
+            return await message.delete()
+             
       button = [[ InlineKeyboardButton(text="Open Whisper Message!", callback_data="whisper_data")]]
       whisper = f"""** 🕵 New Whisper Message!**
       
