3e4d37bfd464a6eb9b58e0aef4da88aa5ebafef3
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-27 17:06:02 +0530

    Update whispers.py

--- nandhabot/plugins/whispers.py
+++ nandhabot/plugins/whispers.py
@@ -14,14 +14,13 @@ Your Not Allow To See Other Personal Messages!**
 @bot.on_message(filters.command("whisper"))
 async def whisper(_, message):
       global user_id
-      
-         mention = message.from_user.meniton
-         if len(message.command) <2:
-                 return await message.reply("ɢɪᴠᴇ  ᴀ  ᴜsᴇʀɪᴅ  ᴡʜᴏ  ᴡᴀɴᴛ  sᴇᴇ  ʏᴏᴜʀ  ʜɪᴅᴅᴇɴ  ᴍᴇssᴀɢᴇ")
-         elif len(message.command) <3:
-               return await message.reply("ɢɪᴠᴇ  ᴍᴇssᴀɢᴇ  ᴛᴏ  ᴄʀᴇᴀᴛᴇ  ᴡʜɪsᴘᴇʀ ᴍᴇssᴀɢᴇ!")
-         user_id = message.text.split(" ")[1]
-         text = message.text.split(" ")[2]
-         button = [[ InlineKeyboardButton(text="Open Whisper Message!", callback_data="whisper_data")]]
-         await bot.send_message(message.chat.id, whisper.format(mention,user_id),reply_markup=InlineKeyboardMarkUp(button))
+      mention = message.from_user.meniton
+      if len(message.command) <2:
+          return await message.reply("ɢɪᴠᴇ  ᴀ  ᴜsᴇʀɪᴅ  ᴡʜᴏ  ᴡᴀɴᴛ  sᴇᴇ  ʏᴏᴜʀ  ʜɪᴅᴅᴇɴ  ᴍᴇssᴀɢᴇ")
+      elif len(message.command) <3:
+          return await message.reply("ɢɪᴠᴇ  ᴍᴇssᴀɢᴇ  ᴛᴏ  ᴄʀᴇᴀᴛᴇ  ᴡʜɪsᴘᴇʀ ᴍᴇssᴀɢᴇ!")
+      user_id = message.text.split(" ")[1]
+      text = message.text.split(" ")[2]
+      button = [[ InlineKeyboardButton(text="Open Whisper Message!", callback_data="whisper_data")]]
+      await bot.send_message(message.chat.id, whisper.format(mention,user_id),reply_markup=InlineKeyboardMarkUp(button))
          
