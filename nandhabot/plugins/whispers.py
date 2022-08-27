b279a8dff708ff5d6d8373bae712113505f3f936
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-27 18:22:46 +0530

    Update whispers.py

--- nandhabot/plugins/whispers.py
+++ nandhabot/plugins/whispers.py
@@ -7,7 +7,8 @@ from nandhabot import bot
 
 @bot.on_message(filters.command("whisper"))
 async def whisper(_, message):
-      global user_id
+      global user_id, name, text
+      name = message.from_user.first_name
       mention = message.from_user.mention
       if len(message.command) <2:
           return await message.reply("ɢɪᴠᴇ  ᴀ  ᴜsᴇʀɪᴅ  ᴡʜᴏ  ᴡᴀɴᴛ  sᴇᴇ  ʏᴏᴜʀ  ʜɪᴅᴅᴇɴ  ᴍᴇssᴀɢᴇ")
@@ -16,7 +17,8 @@ async def whisper(_, message):
       user_id = message.text.split(" ")[1]
       text = message.text.split(" ")[2]
       button = [[ InlineKeyboardButton(text="Open Whisper Message!", callback_data="whisper_data")]]
-      whisper = f"""**🕵 New Whisper Message!**
+      whisper = f"""** 🕵 New Whisper Message!**
+      
 **From User:** {mention}
 **To UserID:** `{user_id}`
 
@@ -26,3 +28,12 @@ Your Not Allow To See Other Personal Messages!**
       await bot.send_message(message.chat.id,whisper,
                reply_markup=InlineKeyboardMarkup(button))
          
+@bot.on_callback_query(filters.regex("whisper_data")
+async def whisperdata(_, query):
+       user = await bot.get_users(user_id)
+       if message.from.user.id == user_id:
+          WHISPER = f"""hey! {user.first_name},
+          here your message from {name} Message: {text}"""
+          await query.answer(WHISPER, show_alert=True)
+       else:
+           await query.answer("YOUR NOT ALLOWED")
