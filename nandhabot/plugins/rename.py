3f7cb8dce9441b546e029bff583729bbd858976d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-28 22:54:34 +0530

    Create rename.py

---
+++ nandhabot/plugins/rename.py
@@ -0,0 +1,17 @@
+from pyrogram import filters
+from pyrogram.types import *
+
+from nandhabot import bot
+
+
+@bot.on_message(filters.command("rename"))
+async def rename(_, message):
+             if not message.repy_to_message and not message.reply_to_message.media:
+                     return await message.reply("reply to media's")
+             elif len(message.command) <2:
+                  return await message.reply("`provide some text with in extinction!\n for example: '/rename movies.mkv`")
+             name = message.text.split(None, 1)[1]
+             elif message.reply_to_message.media:
+                 downloads = await message.reply_to_message.download(file_name=name)
+                 await message.reply_document(downloads)
+           
