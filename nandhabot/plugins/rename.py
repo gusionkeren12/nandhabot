98b8172f3aa99b33a6fb02e5d906e154fefe5708
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-28 22:55:35 +0530

    Update rename.py

--- nandhabot/plugins/rename.py
+++ nandhabot/plugins/rename.py
@@ -11,7 +11,7 @@ async def rename(_, message):
              elif len(message.command) <2:
                   return await message.reply("`provide some text with in extinction!\n for example: '/rename movies.mkv`")
               name = message.text.split(None, 1)[1]
-              elif message.reply_to_message.media:
+              if message.reply_to_message.media:
                  downloads = await message.reply_to_message.download(file_name=name)
                  await message.reply_document(downloads)
            
