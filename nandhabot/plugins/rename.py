baeb6b99d447d3787248c1d97b3401819d09902b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-29 06:39:36 +0530

    Update rename.py

--- nandhabot/plugins/rename.py
+++ nandhabot/plugins/rename.py
@@ -4,16 +4,17 @@ from pyrogram.types import *
 from nandhabot import bot
 
 
+
 @bot.on_message(filters.command("rename"))
 async def rename(_, message):
              if not message.reply_to_message and not message.reply_to_message.media:
                      return await message.reply("reply to media's")
              elif len(message.command) <2:
-                  return await message.reply("provide some text with in extinction!\n for example: '/rename movies.mkv`")
+                  return await message.reply("provide some text with in extinction!\n for example: `/rename movies.mkv`")
              name = message.text.split(None, 1)[1]
              if message.reply_to_message.media:
-                 msg = await message.reply("`downloading...`")
+                 msg = await message.reply("`now downloading...`")
                  downloads = await message.reply_to_message.download(file_name=name)
                  await msg.edit("`download complete!\n now uploading to telegram")
                  return await message.reply_document(downloads)
-           
+                 await msg.delete()
