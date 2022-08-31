2c38083254d156449bd0e4b1c4208b44be20338f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-31 18:33:07 +0530

    Update memify.py

--- nandhabot/plugins/memify.py
+++ nandhabot/plugins/memify.py
@@ -77,7 +77,7 @@ async def drawText(image_path, text):
 
     else:
 
-        fnt = "./FallenRobot/resources/default.ttf"
+        fnt = "./nandhabot/nandha/fonts/logofont.otf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
