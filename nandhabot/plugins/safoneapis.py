6867c6c45d559ed62ceed7e950ca2b10b9f1a227
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-31 09:50:17 +0530

    Create safoneapis.py

---
+++ nandhabot/plugins/safoneapis.py
@@ -0,0 +1,17 @@
+from pyrogram import filters
+from nandhabot import bot
+from SafoneAPI import SafoneAPI
+api = SafoneAPI()
+
+
+
+@bot.on_message(filters.command("app"))
+async def apps(_, message):
+           if len(message.command) <2:
+                retrun await message.reply("**Give A App Name**\n**- /app telegram**")
+           text = message.text.split(None, 1)[1]
+           apps = await api.apps(text)
+           app_text = ""
+           for app in apps.results:
+                   app_text += f" ~ [{app.title}]({app.link})\n"
+           await message.reply_text(app_text)
