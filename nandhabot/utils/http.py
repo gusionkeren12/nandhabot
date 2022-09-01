3144538678eb3a3e1e666c32963e406341e98e12
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-01 20:10:35 +0530

    Update http.py

--- nandhabot/utils/http.py
+++ nandhabot/utils/http.py
@@ -8,3 +8,11 @@ async def get(url: str, *args, **kwargs):
         except Exception:
             data = await resp.text()
     return data
+
+async def post(url: str, *args, **kwargs):
+    async with session.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
