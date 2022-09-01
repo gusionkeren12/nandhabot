48ee8b27cbfae96384b13372b2fd07dcc99bc199
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-02 00:04:50 +0530

    Create animes.py
    Kk

---
+++ nandhabot/plugins/animes.py
@@ -0,0 +1,15 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import requests 
+from pyrogram import filters
+from nandhabot import bot
+
+anews = requests.get("https://api.safone.tech/anime/news").json() 
+
+
+@bot.on_message(filters.command("anews"))
+async def animenews(_, message):
+       caption = anews["results"][0]['description']
+       img = anews["results"][0]["imageUrl"]
+       link = anews["results"][0]["link"]
+       title = anews["results"][0]["title"] 
+       await message.reply_photo(img,caption=caption,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Source Link", url=link)]]))
