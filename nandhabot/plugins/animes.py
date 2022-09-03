705d144a95a111cce7bd14a350fc58b1dfb5171a
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-04 01:04:44 +0530

    Update animes.py

--- nandhabot/plugins/animes.py
+++ nandhabot/plugins/animes.py
@@ -3,14 +3,34 @@ import requests
 from pyrogram import filters
 from nandhabot import bot
 
-anews = requests.get("https://api.safone.tech/anime/news").json() 
+
+
+
 
 
 @bot.on_message(filters.command("anews"))
 async def animenews(_, message):
+       global limit
+       limit = 1
+       api = f"https://api.safone.tech/anime/news?limit={limit}"
+       anews = requests.get(api).json() 
        caption = anews["results"][0]['description']
        img = anews["results"][0]["imageUrl"]
        link = anews["results"][0]["link"]
        title = anews["results"][0]["title"] 
        caption = f"""**Title**: `{title}`\n\n**News**: `{caption}`"""
-       await message.reply_photo(img,caption=caption,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Source Link", url=link)]]))
+       await message.reply_photo(img,caption=caption,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Source Link", url=link),
+             InlineKeyboardButton(text="Next", callback_data="animenews")]]))
+
+@bot.on_callback_query(filters.regex("animenews"))
+async def animenews(_, query):
+       next = limit+1
+       api = f"https://api.safone.tech/anime/news?limit={limit}"
+       anews = requests.get(api).json() 
+       caption = anews["results"][0]['description']
+       img = anews["results"][0]["imageUrl"]
+       link = anews["results"][0]["link"]
+       title = anews["results"][0]["title"] 
+       caption = f"""**Title**: `{title}`\n\n**News**: `{caption}`"""
+       await query.message.edit_media(
+          media=inputMediaPhoto(img,caption)
