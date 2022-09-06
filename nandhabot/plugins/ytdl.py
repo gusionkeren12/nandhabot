cc3da50b900ef8ce68920805f93ba357f6a24a7f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-06 23:54:27 +0530

    Update ytdl.py

--- nandhabot/plugins/ytdl.py
+++ nandhabot/plugins/ytdl.py
@@ -13,7 +13,7 @@ from pyrogram.types import *
 from nandhabot import bot
 
 
-@bot.on_message(filters.command("yt"))
+@bot.on_message(filters.command("ytvideo"))
 async def vsong(client, message):
     ydl_opts = {
         "format": "best",
