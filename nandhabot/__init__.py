a6ccf6906457770aba0afd61a6aef79803b90b8c
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-07 22:22:42 +0530

    Update __init__.py

--- nandhabot/__init__.py
+++ nandhabot/__init__.py
@@ -43,6 +43,16 @@ API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 WEBHOOK = bool(os.environ.get('WEBHOOK', False))
 
+print("""
+
+┏━┓╋┏┓╋╋╋╋╋╋╋┏┳┓╋╋╋╋┏━━┓╋╋╋┏┓
+┃┃┗┓┃┃╋╋╋╋╋╋╋┃┃┃╋╋╋╋┃┏┓┃╋╋┏┛┗┓
+┃┏┓┗┛┣━━┳━┓┏━┛┃┗━┳━━┫┗┛┗┳━┻┓┏╋━━┓
+┃┃┗┓┃┃┏┓┃┏┓┫┏┓┃┏┓┃┏┓┃┏━┓┃┏┓┃┃┃━━┫
+┃┃╋┃┃┃┏┓┃┃┃┃┗┛┃┃┃┃┏┓┃┗━┛┃┗┛┃┗╋━━┃
+┗┛╋┗━┻┛┗┻┛┗┻━━┻┛┗┻┛┗┻━━━┻━━┻━┻━━┛
+by @NandhaBots - @NandhaxD
+"""
 bot = Client("nandhabot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
 tbot = TelegramClient("Vegeta", API_ID, API_HASH)
 
