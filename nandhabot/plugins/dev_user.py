ee65d5e80ef1ca33e9cf38732bb80eb03dedd890
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-08 00:06:37 +0530

    Update dev_user.py

--- nandhabot/plugins/dev_user.py
+++ nandhabot/plugins/dev_user.py
@@ -11,7 +11,7 @@ import pyrogram
 StartTime = time.time()
 import traceback
 from subprocess import getoutput as run
-from nandhabot.plugins.stats import get_users get_groups
+from nandhabot.plugins.stats import get_users, get_groups
 from pyrogram import filters
 from pyrogram.types import (
     CallbackQuery,
