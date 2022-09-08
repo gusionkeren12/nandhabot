81c892df06f7b74ac33bcab4c61d2268ecb10465
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-08 23:35:23 +0530

    Update dbfunctions.py

--- nandhabot/utils/dbfunctions.py
+++ nandhabot/utils/dbfunctions.py
@@ -1,22 +1,14 @@
 from typing import Dict
-from nandhabot import db
+from nandhabot import 
 
-restart_stagedb = db.restart_stage
 
-async def clean_restart_stage() -> dict:
-    data = await restart_stagedb.find_one({"something": "something"})
-    if not data:
-        return {}
-    await restart_stagedb.delete_one({"something": "something"})
-    return {
-        "chat_id": data["chat_id"],
-        "message_id": data["message_id"],
-    }
+
 async def get_sudoers() -> list:
     sudoers = await sudoersdb.find_one({"sudo": "sudo"})
     if not sudoers:
         return []
     return sudoers["sudoers"]
+
 
 
 async def add_sudo(user_id: int) -> bool:
