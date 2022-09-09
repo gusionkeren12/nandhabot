9c52b82f1d2c47b2fefff2bb8656e3cc6b44e1ff
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-09 07:49:05 +0530

    Update __init__.py

--- nandhabot/__init__.py
+++ nandhabot/__init__.py
@@ -46,6 +46,24 @@ API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 WEBHOOK = bool(os.environ.get('WEBHOOK', False))
 
+async def load_sudoers():
+    global SUDOERS
+    print("[INFO]: LOADING SUDOERS")
+    sudoersdb = mongodb.sudoers
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    sudoers = [] if not sudoers else sudoers["sudoers"]
+    for user_id in SUDOERS:
+        if user_id not in sudoers:
+            sudoers.append(user_id)
+            await sudoersdb.update_one(
+                {"sudo": "sudo"},
+                {"$set": {"sudoers": sudoers}},
+                upsert=True,
+            )
+
+    SUDOERS = (SUDOERS + sudoers) if sudoers else SUDOERS
+    print("[INFO]: LOADED SUDOERS")
+
 print("""
 ┏━┓╋┏┓╋╋╋╋╋╋╋┏┳┓╋╋╋╋┏━━┓╋╋╋┏┓
 ┃┃┗┓┃┃╋╋╋╋╋╋╋┃┃┃╋╋╋╋┃┏┓┃╋╋┏┛┗┓
