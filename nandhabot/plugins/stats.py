62f51ce38fc08fb5fad08814ff71b354c5f12a99
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-09 22:39:10 +0530

    Update stats.py

--- nandhabot/plugins/stats.py
+++ nandhabot/plugins/stats.py
@@ -97,7 +97,7 @@ NEW_GROUP = """**New Group Added Our Bot**!
 @bot.on_message(filters.command("addgroup"))
 async def addgroup(_, m):
        await add_group(m.chat.id)
-       await m.repy("**New group added"))
+       await m.repy("**New group added")
 
 @bot.on_message(filters.new_chat_members)
 async def new_chat(_, message):
